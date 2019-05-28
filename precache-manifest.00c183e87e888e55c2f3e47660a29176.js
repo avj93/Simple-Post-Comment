7732244675b2b0efaa7f0a134d240163a4cfb344
Anitha <anithavjohn@gmail.com>
2019-05-28 23:01:32 +0530

    Updates

---
+++ precache-manifest.00c183e87e888e55c2f3e47660a29176.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0533eefa06b00d459573b064dd23ab29",
+    "url": "/Simple-Post-Comment/index.html"
+  },
+  {
+    "revision": "eeab13be08d8755938dd",
+    "url": "/Simple-Post-Comment/static/css/main.18fe31a7.chunk.css"
+  },
+  {
+    "revision": "5c314b27bfe7e1e00d8a",
+    "url": "/Simple-Post-Comment/static/js/2.68d61d40.chunk.js"
+  },
+  {
+    "revision": "eeab13be08d8755938dd",
+    "url": "/Simple-Post-Comment/static/js/main.9e270ad3.chunk.js"
+  },
+  {
+    "revision": "c651ed607691559e03b1",
+    "url": "/Simple-Post-Comment/static/js/runtime~main.6d432ec6.js"
+  }
+]);
